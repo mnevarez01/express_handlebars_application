4518f977b064c3764c5d9e3083b24e27b0687fe7
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-30 14:56:46 -0600

    udpated seeds and schema

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+const connection = require("./connection.js");
+
+
+const orm = {
+
+    //    find all burgers
+
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("Bacon Burger")
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("Avocado Burger")
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("Vegan Burger")
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("Blue Cheese Burger")
