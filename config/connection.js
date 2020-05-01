a29c8fa5a480a3433ecf0b769e972c74449501be
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-30 20:51:24 -0600

    udpated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,28 @@
 var mysql = require("mysql");
 var connection;
 
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "IRene060464!",
+    database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.send.JAWSDB_URL):
+    connection = mysql.createConnection(process.send.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
