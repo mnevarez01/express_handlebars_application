875ded7ed41fd0f2a13c818b8486f9f9ba1e3dee
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-30 18:30:54 -0600

    added burger.js and server.js

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm");
+
+var burger = {
+    all: (cb) => {
+        orm.all("burgers", res => {
+            cb(res);
+        });
+    },
+    create: (name, cb) => {
+        orm.create("burgers", ["burger_name", "devoured"],
+            [
+                name, false
+            ], cb);
+    },
+    update: (id, cb) => {
+        var condition = "id=" + id;
+        orm.update("burgers", {
+            devoured: true
+        }, condition, cb);
+    }
+}
+module.exports = burger;
