2d9c5f3c457a9aa015b2303f0bf5c711fe3f099e
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-30 18:52:39 -0600

    added two get routes

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,3 +1,13 @@
 var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger");
+
+router.get("/", (req, res) => {
+    res.redirect("/burgers");
+});
+
+router.get("/burgers", (req, res) => {
+    burgers.all(burgerData){
+        res.render("index", { burger_data: burgerData })
+    }
+})
