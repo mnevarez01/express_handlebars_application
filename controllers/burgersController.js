adb5955fdd4631f107d7d50d9f8bba19e6e38331
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-30 18:57:44 -0600

    added put controller in file

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -16,5 +16,14 @@ router.post("/burgers/create", (req, res) => {
     burger.create(req.body.burger_name, (result) => {
         console.log(result);
         res.redirect("/");
-    })
-})
+    });
+});
+
+router.put("/burgers/:id", (req, res) => {
+    burger.update(req.params.id, (result) => {
+        console.log(result);
+        res.sendStatus(200);
+    });
+});
+
+module.exports = router; 
